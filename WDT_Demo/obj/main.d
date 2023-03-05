97ff6a9d03142149ea1b64932b5f09f5a066fdea
Christo Smallwood <christo.smallwood@gmail.com>
2023-03-05 21:07:17 +0000

    Watch Dog Timer (WDT) example working on Arm Compiler 6.19
     Changes to be committed:
    	new file:   WDT_Demo/BasicUI.uvguix.Christo
    	new file:   WDT_Demo/BasicUI.uvopt
    	new file:   WDT_Demo/BasicUI.uvoptx
    	new file:   WDT_Demo/BasicUI.uvprojx
    	new file:   WDT_Demo/EventRecorderStub.scvd
    	new file:   WDT_Demo/JLinkSettings.ini
    	new file:   WDT_Demo/RTE/Device/MKL25Z128xxx4/startup_MKL25Z4.s
    	new file:   WDT_Demo/RTE/Device/MKL25Z128xxx4/startup_MKL25Z4.s.base@2.5.0
    	new file:   WDT_Demo/RTE/Device/MKL25Z128xxx4/system_MKL25Z4.c
    	new file:   WDT_Demo/RTE/Device/MKL25Z128xxx4/system_MKL25Z4.c.base@2.5.0
    	new file:   WDT_Demo/RTE/Device/MKL25Z128xxx4/system_MKL25Z4.h
    	new file:   WDT_Demo/RTE/Device/MKL25Z128xxx4/system_MKL25Z4.h.base@2.5.0
    	new file:   WDT_Demo/RTE/_KL25Z_Flash/RTE_Components.h
    	new file:   WDT_Demo/inc/COP_WDT.h
    	new file:   WDT_Demo/inc/GPIO_defs.h
    	new file:   WDT_Demo/inc/LEDs.h
    	new file:   WDT_Demo/inc/config.h
    	new file:   WDT_Demo/inc/delay.h
    	new file:   WDT_Demo/inc/i2c.h
    	new file:   WDT_Demo/inc/mma8451.h
    	new file:   WDT_Demo/lst/BasicUI.map
    	new file:   WDT_Demo/lst/startup_mkl25z4.lst
    	new file:   WDT_Demo/obj/BasicUI.axf
    	new file:   WDT_Demo/obj/BasicUI.build_log.htm
    	new file:   WDT_Demo/obj/BasicUI.htm
    	new file:   WDT_Demo/obj/BasicUI.lnp
    	new file:   WDT_Demo/obj/BasicUI.sct
    	new file:   WDT_Demo/obj/BasicUI_KL25Z Flash.dep
    	new file:   WDT_Demo/obj/ExtDll.iex
    	new file:   WDT_Demo/obj/cop_wdt.d
    	new file:   WDT_Demo/obj/delay.d
    	new file:   WDT_Demo/obj/i2c.d
    	new file:   WDT_Demo/obj/leds.d
    	new file:   WDT_Demo/obj/main.d
    	new file:   WDT_Demo/obj/mma8451.d
    	new file:   WDT_Demo/obj/startup_mkl25z4.d
    	new file:   WDT_Demo/obj/system_mkl25z4.d
    	new file:   WDT_Demo/pemicro_connection_settings.ini
    	new file:   WDT_Demo/src/COP_WDT.c
    	new file:   WDT_Demo/src/LEDs.c
    	new file:   WDT_Demo/src/delay.c
    	new file:   WDT_Demo/src/i2c.c
    	new file:   WDT_Demo/src/main.c
    	new file:   WDT_Demo/src/mma8451.c
    	new file:   WDT_Demo/src/startup_MKL25Z4.s
    	new file:   WDT_Demo/src/system_MKL25Z4.c
    	new file:   WDT_Demo/startup_MKL25Z4.s

---
+++ WDT_Demo/obj/main.d
@@ -0,0 +1,12 @@
+./obj/main.o: src\main.c \
+  C:\Users\Christo\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.H \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\Christo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h \
+  C:\Users\Christo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\Christo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\Christo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h inc\gpio_defs.h \
+  inc\config.h inc\LEDs.h inc\i2c.h inc\mma8451.h inc\delay.h \
+  inc\COP_WDT.h
